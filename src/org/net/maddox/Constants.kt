f03fcb6aafac6e64a4327dfc3860f013e4c851c5
Maddox <95894698+MaddoxOSRS@users.noreply.github.com>
2022-03-04 15:58:11 -0500

    Logic Correction

--- src/org/net/maddox/Constants.kt
+++ src/org/net/maddox/Constants.kt
@@ -5,18 +5,16 @@ import org.powbot.api.Tile
 import org.powbot.api.rt4.Npcs
 
 object Constants {
-    var HOSIDIOUS_BANK = Tile(1748, 3599, 0)
-    var FORTHOS_ENTRANCE = Tile(1671, 3569, 0)
-    var DRUID_AREA = Area(Tile(1803, 9957, 0), Tile(1797, 9951, 0))
-    var FORTHOS_DUNGEON = Area(Tile(1791, 9982, 0), Tile(1822, 9916, 0))
-    var HOSIDIOUS_BANK_AREA = Area(Tile(1745, 3601, 0), Tile(1752, 3594, 0))
-    var DRUID_ATTACK_AREA = Area(Tile(1795, 9957, 0), Tile(1806, 9934, 0))
-    var OUTSIDE_HOUSE = Area(Tile(1737, 3605, 0), Tile(1763, 3509, 0))
-    var FORTHOS_STAIRCASE = Tile(1669, 3567, 0)
-    var NORTHERN_TILE = Tile(1800, 9945, 0)
+    val HOSIDIOUS_BANK = Tile(1748, 3599, 0)
+    val DRUID_AREA = Area(Tile(1803, 9957, 0), Tile(1797, 9951, 0))
+    val FORTHOS_DUNGEON = Area(Tile(1791, 9982, 0), Tile(1822, 9916, 0))
+    val DRUID_ATTACK_AREA = Area(Tile(1795, 9957, 0), Tile(1806, 9934, 0))
+    val OUTSIDE_HOUSE = Area(Tile(1737, 3605, 0), Tile(1763, 3509, 0))
+    val FORTHOS_STAIRCASE = Tile(1669, 3567, 0)
+    val NORTHERN_TILE = Tile(1800, 9945, 0)
     const val FORTHOSENTRANCE = 34865;
     val ALTARS = 34900
-    val ITEMS_TO_LOOT = arrayOf(995, 23499, 1397, 1399, 207).toIntArray()
+    val ITEMS_TO_LOOT = arrayOf(995, 23499, 1397, 1399, 207, 560, 564, 565, 561, 563, 4698, 5295, 5300, 3138, 245, 239).toIntArray()
     const val BoneBoltsID = 8882
     const val RuneArrowID = 892
     const val DRUIDS_ID = 2145
