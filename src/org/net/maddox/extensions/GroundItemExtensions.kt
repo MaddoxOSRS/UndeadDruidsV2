b01835b9c924fb422991f078ab1790ba6569a687
Maddox <95894698+MaddoxOSRS@users.noreply.github.com>
2022-03-04 15:18:11 -0500

    Logic Correction

--- src/org/net/maddox/extensions/GroundItemExtensions.kt
+++ src/org/net/maddox/extensions/GroundItemExtensions.kt
@@ -1,24 +1,8 @@
 package org.net.maddox.extensions
 
 import org.powbot.api.rt4.GrandExchange
-import org.powbot.api.rt4.GroundItem
 import org.powbot.api.rt4.GroundItems
-import org.powbot.api.rt4.GroundItems.stream
-import org.powbot.api.rt4.stream.locatable.interactive.GroundItemStream
 
-fun GroundItems.grounditemPrice(itemID: IntArray): GroundItem {
-    return stream().id(*itemID).first()
-}
-
-fun GroundItems.lootItem(): GroundItemStream {
-    return stream().filtered { GrandExchange.grounditemPrice2(it.id()) >= 1000 }
-    // return stream().GrandExchange.grounditemPrice2(itemID) >= 1000).within(7).isNotEmpty()
-}
-
-fun GroundItem.lootItem2(): GroundItemStream {
-    return stream().filtered { GrandExchange.grounditemPrice2(it.id()) >= 1000 }
-    // return stream().GrandExchange.grounditemPrice2(itemID) >= 1000).within(7).isNotEmpty()
-}
 
 fun GrandExchange.grounditemPrice2(itemID: Int): Int {
     val itemtoPrice = GroundItems.stream().id(itemID).first()
