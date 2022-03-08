a54673c4f50bdb240765eccccecf8ad72d1d29bf
Maddox <95894698+MaddoxOSRS@users.noreply.github.com>
2022-03-08 08:26:29 -0500

    Looting within area fixed, Dodging melee adjusted

--- src/org/net/maddox/Constants.kt
+++ src/org/net/maddox/Constants.kt
@@ -9,9 +9,7 @@ object Constants {
     val FORTHOS_DUNGEON = Area(Tile(1791, 9982, 0), Tile(1822, 9916, 0))
     val DRUID_ATTACK_AREA = Area(Tile(1795, 9957, 0), Tile(1806, 9934, 0))
     val OUTSIDE_HOUSE = Area(Tile(1737, 3605, 0), Tile(1763, 3509, 0))
-    val FORTHOS_STAIRCASE = Tile(1669, 3567, 0)
     val FORTHOS_ENTRANCE = Area(Tile(1669, 3569, 0), Tile(1672, 3564, 0))
-    val NORTHERN_TILE = Tile(1800, 9945, 0)
     const val FORTHOSENTRANCE = 34865
     val ALTARS = 34900
     val ITEMS_TO_LOOT =
--- src/org/net/maddox/extensions/EquipmentExtensions.kt
+++ src/org/net/maddox/extensions/EquipmentExtensions.kt
@@ -5,5 +5,5 @@ import org.powbot.api.rt4.Equipment
 
 fun hasammo(): Boolean {
     return Equipment.itemAt(Equipment.Slot.QUIVER)
-        .id() != Constants.RuneArrowID || Equipment.itemAt(Equipment.Slot.QUIVER).id() != Constants.BoneBoltsID
+        .id() == Constants.RuneArrowID || Equipment.itemAt(Equipment.Slot.QUIVER).id() == Constants.BoneBoltsID
 }
