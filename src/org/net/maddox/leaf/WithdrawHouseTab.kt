6ea71ada8b68d048a4c6ef8b32a5a0a2775cec07
Maddox <95894698+MaddoxOSRS@users.noreply.github.com>
2022-03-08 09:17:30 -0500

    Import organization and package renaming

--- src/org/net/maddox/leaf/WithdrawHouseTab.kt
+++ src/org/net/maddox/leaf/WithdrawHouseTab.kt
@@ -1,9 +1,9 @@
 package org.net.maddox.leaf
 
-import extensions.count
-import extensions.mustWithdrawItem
 import org.net.maddox.Constants
 import org.net.maddox.Script
+import org.net.maddox.extensions.count
+import org.net.maddox.extensions.mustWithdrawItem
 import org.powbot.api.rt4.Bank
 import org.powbot.api.rt4.Inventory
 import org.powbot.api.script.tree.Leaf
